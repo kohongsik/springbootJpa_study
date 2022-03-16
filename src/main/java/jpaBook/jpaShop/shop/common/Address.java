785964fc713790aee592650e63a9d26d97fdfcc9
hongsik <gohongsig@kongsigi.local>
2022-03-16 21:57:55 +0900

    임베디드 값타입 세터 없애고 전체 생성자로만 설정가능하게 수정

--- src/main/java/jpaBook/jpaShop/shop/common/Address.java
+++ src/main/java/jpaBook/jpaShop/shop/common/Address.java
@@ -5,13 +5,19 @@ import lombok.*;
 import javax.persistence.Embeddable;
 
 @Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Embeddable
 public class Address {
+    // (임베디드)값타입은 세터를 없애고 기본 생성자가 모든필드를 담는걸로 해야한다.
     private String city;
     private String street;
     private String zipcode;
+    protected Address () {
+        // jpa 스펙상 기본생성자가 무조건 필요함, 외부 접근 안되도록 만듬
+    }
+    public Address (String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
 }
