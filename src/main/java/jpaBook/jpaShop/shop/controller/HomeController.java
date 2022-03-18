8e9b5a01fa893e2c525ecbc5bd0f0f4dcb51f11e
sigi <sghs3@naver.com>
2022-03-18 15:15:52 +0900

    home html, layout구성

---
+++ src/main/java/jpaBook/jpaShop/shop/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpaBook.jpaShop.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String goHome () {
+        log.info("go home");
+        return "home";
+    }
+}
